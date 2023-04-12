7a07c1cd9563fc5a50e0289389aec1b57781e281
pv08 <40838575+pv08@users.noreply.github.com>
2023-04-12 16:16:41 -0300

    imaginary_functions

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import argparse
 from src.anarede import Anarede
 from src.utils.ana_dict import AnaredeDict
-from src.utils.functions import write_json, write_csv
+from src.utils.functions import *
 
 def main():
     args = argparse.ArgumentParser()
@@ -14,30 +14,55 @@ def main():
     write_json(ana_dict.convertedDBAR, 'DBAR')
     write_json(ana_dict.convertedDLIN, 'DLIN')
 
-    matrix = {i:  [complex(round(0, 4), round(0, 4)) for j in range(0, len(ana_dict.convertedDBAR))] for i in range(0, len(ana_dict.convertedDBAR))}
-    change = ana_dict.convertedDLIN[0].get('from') - 1
-    soma_real = 0
-    soma_img = 0
+    matrix = {i:  [complex(0, 0) for j in range(0, len(ana_dict.convertedDBAR))] for i in range(0, len(ana_dict.convertedDBAR))}
+
     for record in ana_dict.convertedDLIN:
         get_from = record.get('from') - 1
         get_to = record.get('to') - 1
         resistence = record.get('resistence')/100
         reactance = record.get('reactance')/100
-        impedance_real = resistence/ ((resistence ** 2) + (reactance ** 2))
-        impedance_imag = (reactance*(-1))/ ((resistence ** 2) + (reactance ** 2))
-        matrix[get_from][get_to] = complex(round(impedance_real, 4), round(impedance_imag, 4)) * (-1)
-        matrix[get_to][get_from] = complex(round(impedance_real, 4), round(impedance_imag, 4)) * (-1)
-        
-        if get_from != change:
-            matrix[change][change] = complex(round(soma_real, 4), round(soma_img, 4))
-            change = get_from
-            soma_real = 0
-            soma_img = 0
 
-        soma_real += impedance_real
-        soma_img += impedance_imag
-    for bar in matrix.keys():
-        print(f"{bar}: {matrix[bar]}")
+        impedance = return_impedance(resistence=resistence, reactance=reactance)
+        
+        if type(record.get('tap')) == float: #has a transformer
+            impedance = return_transformer_relation(value=impedance, tap=record.get('tap'))
+            matrix[get_from][get_to] += impedance * (-1)
+            matrix[get_to][get_from] += impedance * (-1)
+            
+        else:
+            matrix[get_from][get_to] += impedance * (-1)
+            matrix[get_to][get_from] += impedance * (-1)
+
+    
+    for line, bar in zip(matrix.keys(), ana_dict.convertedDBAR):
+        if type(bar.get('capacitor')) == float:
+            capacitance = return_capacitor(value=bar.get('capacitor'), base=100, tension=bar.get('tension'))
+            matrix[line][line] = (sum(matrix[line]) * (-1)) + capacitance
+        else:
+            matrix[line][line] = sum(matrix[line]) * (-1)
+        for record in ana_dict.convertedDLIN:
+            get_from = record.get('from') - 1
+            get_to = record.get('to') - 1
+
+            resistence = record.get('resistence')/100
+            reactance = record.get('reactance')/100
+            
+            susceptance = return_susceptance_shunt(value=record.get('susceptance'), base=100)
+            impedance = return_impedance(resistence=resistence, reactance=reactance)
+
+            if type(record.get('tap')) == float:
+                if line == get_from:
+                    B = return_transformer_B(value=impedance, tap=record.get('tap'))
+                    matrix[line][line] += susceptance + B
+                if line == get_to:
+                    C = return_transformer_C(value=impedance, tap=record.get('tap'))
+                    matrix[line][line] += susceptance + C
+            else:
+                if line == get_from or line == get_to:
+                    matrix[line][line] += susceptance
+
     write_csv(matrix, 'y_barra')
+        
+        
 if __name__ == "__main__":
     main()
--- src/utils/functions.py
+++ src/utils/functions.py
@@ -5,6 +5,30 @@ def write_json(arr, name):
     with open(f'{name}.json', 'w') as json_file:
         json_file.write(json.dumps(arr, indent=4))
 
+def return_impedance(resistence, reactance):
+    impedance_real = resistence/ ((resistence ** 2) + (reactance ** 2))
+    impedance_imag = (reactance*(-1))/ ((resistence ** 2) + (reactance ** 2))
+    return complex(impedance_real, impedance_imag)
+
+def return_susceptance_shunt(value, base):
+    try:
+        return complex(0, (value/2*base))
+    except:
+        return complex(0, 0)
+
+def return_transformer_relation(value, tap):
+    return value*tap
+
+def return_transformer_B(value, tap):
+    return (tap * (tap - 1)) * value
+
+def return_transformer_C(value, tap):
+    return (1 - tap) * value
+
+def return_capacitor(value, base, tension):
+    return complex(0, (value/base) * tension)
+
+
 def write_csv(dictionary, name):
     df = pd.DataFrame(dictionary)
     df.to_csv(f"{name}.csv", index=False)
